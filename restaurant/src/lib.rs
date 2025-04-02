ad3e2e023f55051030267d0df762b52e9891e567
wessamfathi <wessamfathi@gmail.com>
2025-04-02 20:08:41 +0200

    More module exercises

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,9 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        pub fn seat_at_table() {}
-    }
+mod front_of_house;
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
+mod serving {
+    fn take_order() {}
+    fn serve_order() {}
+    fn take_payment() {}
 }
 
 mod back_of_house {
@@ -40,9 +35,10 @@ mod back_of_house {
 
 fn deliver_order() {}
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // ready to order
     // order a brekfast in summer with rye
